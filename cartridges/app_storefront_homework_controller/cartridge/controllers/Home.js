82fdab014436329e08d928a29fb78c92c4fe674e
BozhidarYordanov <bozhidar.h.yordanov@gmail.com>
2021-11-28 23:42:14 +0200

    Extends the Home-Show endpoint

---
+++ cartridges/app_storefront_homework_controller/cartridge/controllers/Home.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var server = require('server');
+server.extend(module.superModule);
+
+server.append('Show', function (req, res, next) {
+    var viewData = res.getViewData();
+    if (!viewData.product) {
+        next();
+    } else {
+        viewData.product.reviews = [{
+            text: 'Lorem ipsum dolor sit amet, cibo utroque ne vis, has no sumo graece.' +
+            ' Dicta persius his id. Ea maluisset scripserit contentiones quo, est ne movet dicam.' +
+            ' Equidem scriptorem vis no. Civibus tacimates interpretaris has et,' +
+            ' ei offendit ocurreret vis, eos purto pertinax eleifend ea.',
+            rating: 3.5
+        }, {
+            text: 'Very short review',
+            rating: 5
+        }, {
+            text: 'Lorem ipsum dolor sit amet, cibo utroque ne vis, has no sumo graece.',
+            rating: 1.5
+        }];
+
+        res.setViewData(viewData);
+        next();
+    }
+})
+
+module.exports = server.exports();
